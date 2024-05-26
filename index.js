c5e3f2d04f28573b700140a4c10d9a099bf69c80
Dima Prosto <csourgg@gmail.com>
2024-05-26 16:08:38 +0300

    first commit (Complete task)

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+//1) зібрати живу колекцію усіх посилань на сторінці
+const liveCollection = document.getElementsByTagName('a');
+console.log(liveCollection);
+
+// 2) забрати статичну колекцію усіх зображенб на сторінці
+const staticCollection = document.querySelectorAll('img')
+console.log(staticCollection);
+
+// 3) дістати останній елемент li з меню
+const lastMenuItem = document.querySelector('.menu ul').lastElementChild;
+console.log(lastMenuItem);
+
+// 4) дістати заголовок першої статті з секції з класом content-section
+const firstTitle =  document.querySelector('.content-section > article').children[0];
+console.log(firstTitle);
+
+// 5) дістати усі другі абзаци в статтях
+const allSecondParagraphs = document.querySelectorAll('.content-section > article');
+const secondParagraphs = [];
+
+allSecondParagraphs.forEach( article => {
+    const p = article.querySelectorAll('p')
+    if(p.length > 1) {
+        secondParagraphs.push(p[1]);
+    }
+})
+console.log(secondParagraphs);
